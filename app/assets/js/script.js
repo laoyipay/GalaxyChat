20e13ef12cb8efeba2a4c308e9bcf6ab3a72e464
Yi <laoyipay@msn.com>
2022-05-01 16:07:27 +0800

    Integrate speech API into chat

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -838,11 +838,239 @@
     });
 
 
+
+
+
+
+/*=====================
+       27. Speech & Bot
+==========================*/
+
+
+
+// function showInfo(s) {
+//     if (s) {
+//         for (var child = info.firstChild; child; child = child.nextSibling) {
+//             if (child.style) {
+//                 child.style.display = child.id == s ? 'inline' : 'none';
+//             }
+//         }
+//         info.style.visibility = 'visible';
+//     } else {
+//         info.style.visibility = 'hidden';
+//     }
+// }
+
+
+// showInfo('info_start');
+var processing = false;
+
+
+function speak (text) {
+    let utterance = new SpeechSynthesisUtterance(text);
+    utterance.pitch = 1;
+    utterance.rate = 1
+    speechSynthesis.speak(utterance);
+}
+
+
+function requestNPL(text){
+
+    if (processing || text == null || text === "") return;
+
+    processing = true;
+
+    console.log("text",text);
+    text = encodeURIComponent(text.trim());
+    console.log("trimmed text",text);
+
+
+    $.ajax({
+        url: 'https://api.wit.ai/message?v=20220419&q=' + text,
+        type: 'GET',
+        contentType: 'application/json',
+        headers: {
+            'Authorization': 'Bearer E6VQQX5W7HR2AAYYYHQC4G7XOAVTP4BM'
+        },
+        success: function (result) {
+            processing = false;
+            console.log("success");
+            console.log(result);
+
+            var intent = result.intents[0].name;
+            var confinance = result.intents[0].confidence * 100;
+            
+
+            console.log("intenst",intent);
+            console.log("intenst confinance",confinance);
+
+            //speak("Test Speaking")
+
+
+            var coin = result.entities["coin:coin"][0].value;
+            console.log("coin",coin);
+            
+
+        },
+        error: function (error) {
+            processing = false;
+            console.log("error");
+            console.log(error);
+        }
+    });
+}
+
+
+var final_transcript = '';
+var recognizing = false;
+var ignore_onend;
+var start_timestamp;
+if (!('webkitSpeechRecognition' in window)) {
+    upgrade();
+} else {
+    start_button.style.display = 'inline-block';
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+
+    recognition.onstart = function () {
+        recognizing = true;
+        // showInfo('info_speak_now');
+        start_img.src = 'https://google.com/intl/en/chrome/assets/common/images/content/mic-animate.gif';
+    };
+
+    recognition.onerror = function (event) {
+        if (event.error == 'no-speech') {
+            start_img.src = 'https://google.com/intl/en/chrome/assets/common/images/content/mic.gif';
+            // showInfo('info_no_speech');
+            ignore_onend = true;
+        }
+        if (event.error == 'audio-capture') {
+            start_img.src = 'https://google.com/intl/en/chrome/assets/common/images/content/mic.gif';
+            // showInfo('info_no_microphone');
+            ignore_onend = true;
+        }
+        if (event.error == 'not-allowed') {
+            if (event.timeStamp - start_timestamp < 100) {
+                // showInfo('info_blocked');
+            } else {
+                // showInfo('info_denied');
+            }
+            ignore_onend = true;
+        }
+    };
+
+    recognition.onend = function () {
+        recognizing = false;
+        if (ignore_onend) {
+            return;
+        }
+        start_img.src = 'https://google.com/intl/en/chrome/assets/common/images/content/mic.gif';
+        if (!final_transcript) {
+            // showInfo('info_start');
+            return;
+        }
+        // showInfo('');
+        if (window.getSelection) {
+            window.getSelection().removeAllRanges();
+            var range = document.createRange();
+            // range.selectNode(document.getElementById('final_span'));
+            window.getSelection().addRange(range);
+        }
+        
+    };
+
+    recognition.onresult = function (event) {
+        var interim_transcript = '';
+        if (typeof (event.results) == 'undefined') {
+            recognition.onend = null;
+            recognition.stop();
+            upgrade();
+            return;
+        }
+        for (var i = event.resultIndex; i < event.results.length; ++i) {
+            if (event.results[i].isFinal) {
+                final_transcript += event.results[i][0].transcript;
+            } else {
+                interim_transcript += event.results[i][0].transcript;
+            }
+        }
+
+   
+        console.log("final_transcript",final_transcript);
+
+        
+    
+
+        //requestNPL(final_transcript);
+    };
+}
+
+function upgrade() {
+    start_button.style.visibility = 'hidden';
+    // showInfo('info_upgrade');
+}
+
+var two_line = /\n\n/g;
+var one_line = /\n/g;
+function linebreak(s) {
+    return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+}
+
+var first_char = /\S/;
+function capitalize(s) {
+    return s.replace(first_char, function (m) { return m.toUpperCase(); });
+}
+
+
+
+function startButton(event) {
+    console.log("load startButton ");
+    if (recognizing) {
+        recognition.stop();
+        return;
+    }
+    final_transcript = '';
+    recognition.lang = "en-US";
+    console.log(" recognition.lang", recognition.lang);
+    recognition.start();
+    ignore_onend = false;
+   
+    start_img.src = 'https://google.com/intl/en/chrome/assets/common/images/content/mic-slash.gif';
+    // showInfo('info_allow');
+    showButtons('none');
+    start_timestamp = event.timeStamp;
+}
+
+
+$( "#start_button" ).click(function(event) {
+    console.log("click startButton ");
+    startButton(event);
+});
+
+var current_style;
+function showButtons(style) {
+    if (style == current_style) {
+        return;
+    }
+    current_style = style;
+
+}
+
+
+
+
+
+
+
+
+
+
 /*=====================
        26. Web3
 ==========================*/
 
-       if (typeof web3 !== 'undefined') {
+if (typeof web3 !== 'undefined') {
         web3 = new Web3(web3.currentProvider);
         console.log("Metamask installed！ ",web3.currentProvider);
         
@@ -971,6 +1199,10 @@ $( "#send" ).click(function() {
 
 
 })();
+
+
+
+
 
 
 })(jQuery);
